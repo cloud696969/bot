2df007890541f27291589521929bdf541cd2ff59
cloud696969 <109817911+cloud696969@users.noreply.github.com>
2023-09-03 19:29:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ async function getTopUSDTMFutures24H() {
         }
       }
       pairs = pairs.filter((pair) => !blacklist.includes(pair.symbolName));
-      pairs = pairs.filter((pair) => pair.changePercent > 3.5 && pair.changePercent < 6.7);
+      pairs = pairs.filter((pair) => pair.changePercent > 4.5 && pair.changePercent < 12);
 
       const topPositive = pairs
         .sort((a, b) => b.changePercent - a.changePercent)
